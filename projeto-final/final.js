07ded60d5071815af097fefba09976bf205f7b02
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-07-16 19:55:06 -0300

    Fizemos todos o nosso melhor para achar a solução, foi dificil mas conseguimos

--- projeto-final/final.js
+++ projeto-final/final.js
@@ -1,10 +1,8 @@
+
 let lista = [];
-let num = parseInt(prompt("Digite um número:"));
-let num2 = parseInt(prompt("Digite um número:"));
-let num3 = parseInt(prompt("Digite um número:"));
-let num4 = parseInt(prompt("Digite um número:"));
-let num5 = parseInt(prompt("Digite um número:"));
-lista.push(num, num2, num3, num4, num4, num5);
+for(let i = 0; i < 5; i++){
+    lista.push(prompt('Digite um número:'))
+}
 
 let menor = Math.min(...lista);
 let maior = Math.max(...lista);
@@ -13,17 +11,21 @@ document.write(` o maior número da lista é ${maior} e o menor é ${menor} <br>
 
 for (let valor of lista){
     if(valor % 2 == 0){
-        document.write('Par' + valor + '<br>');
+        document.write(`O número ${valor} é  <br>`);
     }else{
-        document.write('impar'+ valor + '<br>');
+        document.write(`O número ${valor} é Impar <br>`);
     }
 }
+    
 
-var cont = 0;
+    var map = lista.reduce(function(prev, cur) {
+    prev[cur] = (prev[cur] || 0) + 1;
+    return prev;
+    }, {});
+    
+    for (let i in map) {
+    document.write(`<p>O número ${i} foi repetido ${map[i]}</p>`)
+    }
+   
+    
 
-for (let valor of lista){
-    if (valor == num || valor == num2 || valor == num3 || valor == num4 || num5){
-        cont++
-}}
-
-document.write(cont - 5 + 'numeros repitidos' + '<br>');
